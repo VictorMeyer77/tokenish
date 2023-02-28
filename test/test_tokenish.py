74a14b23795d6ce6ade6301f9d3455a64848ccc0
VictorMeyer77 <victormeyerc2i@gmail.com>
2023-02-28 20:22:19 +0100

    rename main to tokenish

--- test/test_tokenish.py
+++ test/test_tokenish.py
@@ -1,5 +1,5 @@
 import unittest
-import main
+import tokenish
 import os
 from lib.result_writer import ResultWriter
 
@@ -10,8 +10,8 @@ class TestMain(unittest.TestCase):
         token_paths = ["test/resources/input/links/1.txt",
                        "test/resources/input/usernames",
                        "test/resources/input/passwords"]
-        main.main("Link: &ENC[test]ODE& &TOKEN_0& - Auth:&TOKEN_1&/&TOKEN_2&", token_paths,
-                  None, ResultWriter("test/resources/output", 10000))
+        tokenish.main("Link: &ENC[test]ODE& &TOKEN_0& - Auth:&TOKEN_1&/&TOKEN_2&", token_paths,
+                      None, ResultWriter("test/resources/output", 10000))
         output_path = "test/resources/output/part_0.txt"
         output = open(output_path, "r")
         lines = output.readlines()
@@ -25,8 +25,8 @@ class TestMain(unittest.TestCase):
         token_paths = ["test/resources/input/links/1.txt",
                        "test/resources/input/usernames",
                        "test/resources/input/passwords"]
-        main.main("Link: &ENC[test]ODE& &TOKEN_0& - Auth:&TOKEN_1&/&TOKEN_2&", token_paths,
-                  "base64", ResultWriter("test/resources/output", 10000))
+        tokenish.main("Link: &ENC[test]ODE& &TOKEN_0& - Auth:&TOKEN_1&/&TOKEN_2&", token_paths,
+                      "base64", ResultWriter("test/resources/output", 10000))
         output_path = "test/resources/output/part_0.txt"
         output = open(output_path, "r")
         lines = output.readlines()
@@ -38,13 +38,13 @@ class TestMain(unittest.TestCase):
         os.remove(output_path)
 
     def test_parse_args_return_all_parameters(self):
-        assert main.parse_args(["pattern"]) == ("pattern", [], None, None)
-        assert main.parse_args(["pattern", "-t", "/path/tokenA/", "/path/tokenB.txt"]) == \
+        assert tokenish.parse_args(["pattern"]) == ("pattern", [], None, None)
+        assert tokenish.parse_args(["pattern", "-t", "/path/tokenA/", "/path/tokenB.txt"]) == \
                ("pattern", ["/path/tokenA/", "/path/tokenB.txt"], None, None)
-        assert main.parse_args(["pattern", "-t", "/path/tokenA/", "/path/tokenB.txt", "-e", "base64"]) == \
+        assert tokenish.parse_args(["pattern", "-t", "/path/tokenA/", "/path/tokenB.txt", "-e", "base64"]) == \
                ("pattern", ["/path/tokenA/", "/path/tokenB.txt"], "base64", None)
-        args_with_output = main.parse_args(["pattern", "-o", "/output"])
+        args_with_output = tokenish.parse_args(["pattern", "-o", "/output"])
         assert args_with_output[3].directory_path == ResultWriter("/output", 10000).directory_path
         assert args_with_output[3].max_row_per_file == ResultWriter("/output", 10000).max_row_per_file
-        args_with_output_and_max_rows = main.parse_args(["pattern", "-o", "/output", "-om", "50"])
+        args_with_output_and_max_rows = tokenish.parse_args(["pattern", "-o", "/output", "-om", "50"])
         assert args_with_output_and_max_rows[3].max_row_per_file == ResultWriter("/output", 50).max_row_per_file
