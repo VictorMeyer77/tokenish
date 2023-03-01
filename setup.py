738dda24c61d9598d78a47699dbab03267626092
VictorMeyer77 <victormeyerc2i@gmail.com>
2023-03-01 15:41:48 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="tokenish",
+      version="1.0.0",
+      description="A simple tool to fill pattern with tokens from file or directory",
+      author="Victor Meyer",
+      author_email="victor_meyer@outlook.fr",
+      python_requires=">=3.6, <4",
+      scripts=["tokenish"],
+      )
