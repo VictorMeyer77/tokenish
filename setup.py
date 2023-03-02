7c4f4d6f7163722827f8228ba6cda07dd6fb9902
VictorMeyer77 <victormeyerc2i@gmail.com>
2023-03-02 16:42:49 +0100

    refactor configuration

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-from distutils.core import setup
-
-setup(name="tokenish",
-      version="0.1",
-      description="A simple tool to fill pattern with tokens from file or directory",
-      url="https://github.com/VictorMeyer77/tokenish",
-      author="Victor Meyer",
-      author_email="victor_meyer@outlook.fr",
-      packages=["tokenish", "tokenish.lib"],
-      python_requires=">=3.6, <4",
-      scripts=["bin/tokenish"],
-      license="MIT",
-      classifiers=[
-            "Development Status :: 4 - Beta",
-            "Environment :: Console",
-            "Intended Audience :: Developers",
-            "Intended Audience :: System Administrators",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: POSIX",
-            "Programming Language :: Python",
-            ],
-      )
+from setuptools import setup
+if __name__ == '__main__':
+    setup()
